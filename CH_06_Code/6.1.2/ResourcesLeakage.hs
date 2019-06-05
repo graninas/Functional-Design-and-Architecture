178ce84124586889b57d95f5944a56460dd44120
Alexander Granin <graninas@gmail.com>
2019-06-06 01:05:18 +0700

    Chapter 6 samples (paragraph 6.1).

---
+++ CH_06_Code/6.1.2/ResourcesLeakage.hs
@@ -0,0 +1,52 @@
+module Main where
+
+import Data.IORef (IORef, newIORef, writeIORef, readIORef, modifyIORef)
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever, void)
+import System.Random (randomRIO)
+
+data Meteor = Meteor
+  { size :: Int
+  , mass :: Int
+  }
+  deriving (Show, Read, Eq)
+
+type ReportingChannel = IORef [Meteor]
+
+getRandomMeteor = do
+  rndSize <- randomRIO (1, 100)
+  rndMass <- randomRIO (rndSize * 1, rndSize * 10)
+  pure $ Meteor rndSize rndMass
+
+reportMeteor :: ReportingChannel -> Meteor -> IO ()
+reportMeteor ch meteor = do
+  reported <- readIORef ch
+  writeIORef ch $ meteor : reported
+
+astronomer :: ReportingChannel -> IO ()
+astronomer ch = do
+  rndMeteor <- getRandomMeteor
+  rndDelay <- randomRIO (1000, 10000)
+  reportMeteor ch rndMeteor
+
+  someRef <- newIORef [1..10]
+  forever $ forkIO $ do
+    val <- readIORef someRef
+    print val
+
+  threadDelay rndDelay
+
+trackingCenter :: ReportingChannel -> IO ()
+trackingCenter ch = do
+  reported <- readIORef ch
+  writeIORef ch []
+  print $ "Reported: " <> show (length reported)
+  threadDelay 10000
+
+main :: IO ()
+main = do
+  ch <- newIORef []
+
+  forkIO $ forever $ astronomer ch
+
+  forever $ trackingCenter ch
