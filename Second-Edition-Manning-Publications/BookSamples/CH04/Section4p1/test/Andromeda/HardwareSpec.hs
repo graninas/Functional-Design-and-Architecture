879187aae8b30c4be564dc283c3f2c255a5e87a9
Alexander Granin <graninas@gmail.com>
2021-08-12 00:45:57 +0700

    Code for the support video #1

--- Second-Edition-Manning-Publications/BookSamples/CH04/Section4p1/test/Andromeda/HardwareSpec.hs
+++ Second-Edition-Manning-Publications/BookSamples/CH04/Section4p1/test/Andromeda/HardwareSpec.hs
@@ -8,6 +8,73 @@ import Andromeda
 import Andromeda.Assets (boostersDef, aaaController86Name)
 import Andromeda.Assets.Vendors.AAA.ComponentsAPI (aaaVendorComponents)
 
+
+
+-- Drafting the domain (Domain-Driven Design in Haskell)
+data LogicMethod = LogicMethod
+
+type Logic = [LogicMethod]
+
+
+data LogicControlMethod
+  = HdlScript Hdl
+  | LogicScript Logic
+
+
+type LogicControl = [LogicControlMethod]
+
+
+-- monitorTemperature :: DevicePart -> m ()
+-- monitorTemperature d = do
+--   measurement <- withHandler d readMeasurement
+--   t <- validate Temperature measurement
+--   if t > 100.0
+--     then alarm "Temperature exceeds critical!"
+--     else pure ()
+--
+--
+--
+-- logicControlScript :: LogicControl ()
+-- logicControlScript = do
+--   boosters <- makeDevice boostersDef
+--   therm <- getDevicePart "nozzle1-t" boosters
+--
+--   periodically 1.0 $ monitorTemperature therm
+--
+
+data LogicMethod = LogicMethod
+
+type Logic = [LogicMethod]
+
+data LogicControlMethod
+  = forall a. HdlScript (Hdl a) (a -> LogicControl)
+  | LogicScript Logic
+
+type LogicControl = [LogicControlMethod]
+
+logicControlScript :: LogicControl
+
+------------
+
+data Handle = Handle
+  { makeDevice :: Hdl -> Device
+  , getBlankDeivce :: ...
+  }
+
+---------- ---
+-- Domain & BL
+-- Hdl, Hndl, Scripts, ....
+
+----  --------
+-- Impl
+-- Device
+-- Device interop
+
+
+
+
+
+
 spec :: Spec
 spec =
   describe "Hardware tests" $ do
